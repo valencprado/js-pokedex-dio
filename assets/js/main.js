859f43f31d1cbc9d791313fd8c1b9beae3d5e7cb
Valen <valenpschool@gmail.com>
2023-05-04 12:49:34 -0300

    refacto: simplificação do código

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,13 +20,7 @@ function convertPokemonToLi(pokemon) {
 }
 
 const pokemonList = document.getElementById('pokemonList');
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => jsonBody.results)
-    .then((pokemons) => {
-        for (let i = 0; i < pokemons.length; i++) {
-            const pokemon = pokemons[i];
-            pokemonList.innerHTML += convertPokemonToLi(pokemon);
-        }
-    })
+pokeApi.getPokemons().then((pokemons = []) => {
+    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join("");
+})
     .catch((err) => console.log(err));
