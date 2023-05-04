28f58b4dcce793d8246554a156e845b9603f4b18
Valen <valenpschool@gmail.com>
2023-05-04 13:20:15 -0300

    feat: detalhes dos pokémons

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,9 +1,18 @@
+const offset = 0;
+const limit = 5;
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 const pokeApi = {}
 
+pokeApi.getPokemonDetails = (pokemon) => {
+    return fetch(pokemon.url)
+        .then((response) => response.json())
+}
 pokeApi.getPokemons = (offset = 0, limit = 10) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
     return fetch(url)
         .then((response) => response.json())
         .then((jsonBody) => jsonBody.results)
-        .catch((err) => console.log(err));
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetails))
+        .then((detailRequests) => Promise.all(detailRequests))
+        .then((pokemonsDetails) => (pokemonsDetails))
 }
