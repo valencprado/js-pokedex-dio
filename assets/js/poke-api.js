4c4c4385defd8ef2bb0149cf0b85a8fb6cb8a158
Valen <valenpschool@gmail.com>
2023-05-05 11:52:15 -0300

    alterando para o modelo

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -3,9 +3,21 @@ const limit = 5;
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 const pokeApi = {}
 
+function convertPokeApiDetailToPokemon(pokeDetail) {
+    const pokemon = new Pokemon();
+    pokemon.number = pokeDetail.order;
+    pokemon.name = pokeDetail.name;
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+    pokemon.types = types;
+    pokemon.type = type;
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default;
+    return pokemon;
+}
 pokeApi.getPokemonDetails = (pokemon) => {
     return fetch(pokemon.url)
         .then((response) => response.json())
+        .then(convertPokeApiDetailToPokemon);
 }
 pokeApi.getPokemons = (offset = 0, limit = 10) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
